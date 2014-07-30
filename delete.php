3371675aaa6a3e6be478ea07b217bf46609ceafe
Alan Bell <alan.bell@libertus.co.uk>
2014-07-30 11:23:54 +0100

    delete of objects first pass

---
+++ delete.php
@@ -0,0 +1,7 @@
+<?php
+//this will delete objects from the user interface, it is going to check permissions when we have permissions
+//and won't really delete things, but puts them in a deleted collection
+sleep(2);
+print_r( $_POST);
+
+?>
